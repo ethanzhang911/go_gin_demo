8b6f7ac70870fcf2377fe2d110865626d12faf23
Administrator <21802259@qq.com>
2021-05-29 18:44:29 +0800

    0.01

--- main.go
+++ main.go
@@ -9,5 +9,17 @@ func main() {
        	"message":"pong",
 	   })
 	})
+	//get方法
+	r.GET("/get", func(c *gin.Context) {
+		c.String(200,"get")
+	})
+	r.POST("/post", func(c *gin.Context) {
+		c.String(200,"post")
+	})
+
+	//注意这里httpMethod是大写DELETE
+	r.Handle("DELETE","/delete", func(c *gin.Context) {
+		c.String(200,"delete")
+	})
 	r.Run()
 }
